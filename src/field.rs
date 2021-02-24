1e21e186eac94dd26cfe8931f3c3b8ef48c5f074
Konstantin Tenzin <tenzink@yandex.ru>
2021-02-24 09:52:41 +0300

    Add algebraic notation for the fields

--- src/field.rs
+++ src/field.rs
@@ -71,3 +71,32 @@ pub const G8: usize = 62;
 pub const H8: usize = 63;
 
 pub const COUNT: usize = 64;
+
+#[rustfmt::skip]
+const NAMES: [&'static str; 64] = [
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+];
+
+pub fn algebraic(idx: usize) -> &'static str {
+    NAMES[idx]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_moves_on_empty_board() {
+        assert_eq!(algebraic(A1), "a1");
+        assert_eq!(algebraic(E6), "e6");
+        assert_eq!(algebraic(C4), "c4");
+        assert_eq!(algebraic(F3), "f3");
+    }
+}
